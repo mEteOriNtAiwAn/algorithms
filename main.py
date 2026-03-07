dcadd75abba73d622660969372a2d2bd5b85de8f
Pizduk-Lenia <danil.tikhomirov.06@inbox.ru>
2026-03-07 06:45:06 +0300

    commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("dada")
+def all_unique(list_num:list)->bool:
+    dict_value = {}
+    for num in list_num:
+        val = dict_value.setdefault(num, 0)
+        if(val != 0): return False
+        dict_value[num] += 1
+
+    return True
+
+print(all_unique([3,2,3,4]))
